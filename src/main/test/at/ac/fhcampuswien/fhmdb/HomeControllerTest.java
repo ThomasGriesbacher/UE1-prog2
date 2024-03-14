601c57d622d3a8c8831d95bb3f76a7f67363d575
trimxa <kushtrim.muriqi@stud.fh-campuswien.ac.at>
2024-03-14 23:43:19 +0100

    test test

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,30 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class HomeControllerTest {
+public class HomeControllerTest {
+    private final HomeController controller = new HomeController();
 
+    @Test
+    void sortAscending(){
+        List<Movie> expected = new ArrayList<>();
+        expected.add(new Movie("title1", "description1", ADVENTURE));
+        expected.add(new Movie("title2", "description2", BIOGRAPHY));
+        expected.add(new Movie("title3", "description3", CRIME));
+        controller.sortAscending();
+        assertEquals(expected, controller.filteredMovies);
+    }
+
+    @Test
+    void sortDescending(){
+        List<Movie> expected = new ArrayList<>();
+        expected.add(new Movie("title3", "description3", CRIME));
+        expected.add(new Movie("title2", "description2", BIOGRAPHY));
+        expected.add(new Movie("title1", "description1", ADVENTURE));
+        controller.sortDescending();
+        assertEquals(expected, controller.filteredMovies);
+    }
 }
