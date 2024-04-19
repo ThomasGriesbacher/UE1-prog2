2315831dcbaacb9b3a90317168546fa94d9c9cbc
Thomas G <norbert.griesbacher@gmail.com>
2024-04-19 16:14:03 +0200

    HomeControllerTest class

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,71 +1,324 @@
 package at.ac.fhcampuswien.fhmdb;
+
 import at.ac.fhcampuswien.fhmdb.models.Genre;
-import at.ac.fhcampuswien.fhmdb.models.*;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeControllerTest {
-    private final HomeController controller = new HomeController();
 
-   /*@Test
-    void sortAscending(){
-        List<Movie> expected = new ArrayList<>();
-        expected.add(new Movie("title1", "description1", Genre.ACTION));
-        expected.add(new Movie("title2", "description2", Genre.ADVENTURE));
-        expected.add(new Movie("title3", "description3", Genre.BIOGRAPHY));
-        expected.add(new Movie("title4", "description4", Genre.COMEDY));
-        expected.add(new Movie("title5", "description5", Genre.CRIME));
-        expected.add(new Movie("title6", "description6", Genre.DRAMA));
-        expected.add(new Movie("title7", "description7", Genre.DOCUMENTARY));
-        expected.add(new Movie("title8", "description8", Genre.FAMILY));
-        expected.add(new Movie("title9", "description9", Genre.FANTASY));
-        expected.add(new Movie("title10", "description10", Genre.HISTORY));
-        expected.add(new Movie("title11", "description11", Genre.HORROR));
-        expected.add(new Movie("title12", "description12", Genre.MUSICAL));
-        expected.add(new Movie("title13", "description13", Genre.MYSTERY));
-        expected.add(new Movie("title14", "description14", Genre.ROMANCE));
-        expected.add(new Movie("title15", "description15", Genre.SCIENCE_FICTION));
-        expected.add(new Movie("title16", "description16", Genre.SPORT));
-        expected.add(new Movie("title17", "description17", Genre.THRILLER));
-        expected.add(new Movie("title18", "description18", Genre.WAR));
-        expected.add(new Movie("title19", "description19", Genre.WESTERN));
-        controller.sortAscending();
-        assertEquals(expected, controller.filteredMovies);
+
+    @Test
+    public void testGetMostPopularActor_EmptyList() {
+        List<Movie> movies = new ArrayList<>();
+        String mostPopularActor = HomeController.getMostPopularActor(movies);
+        assertEquals("", mostPopularActor);
+    }
+
+
+    @Test
+    public void testGetMostPopularActor_SingleMovie() {
+
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+        ArrayList<String> directors = new ArrayList();
+        directors.add("Director 1");
+        directors.add("Director 2");
+
+        ArrayList<String> writers = new ArrayList();
+        writers.add("Writer 1");
+        writers.add("Writer 2");
+
+
+        ArrayList<String> cast = new ArrayList<>();
+        cast.add("Actor A");
+        cast.add("Actor B");
+        cast.add("Actor A");
+
+        Movie movie = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast,
+                10);
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie);
+
+        String mostPopularActor = HomeController.getMostPopularActor(movies);
+        assertEquals("Actor A", mostPopularActor);
     }
 
     @Test
-    void sortDescending(){
-        List<Movie> expected = new ArrayList<>();
-        expected.add(new Movie("title3", "description3", Genre.CRIME));
-        expected.add(new Movie("title2", "description2", Genre.BIOGRAPHY));
-        expected.add(new Movie("title1", "description1", Genre.ADVENTURE));
-        controller.sortDescending();
-        assertEquals(expected, controller.filteredMovies);
+    public void testGetMostPopularActor_MultipleMovies() {
+
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+
+        ArrayList<String> directors = new ArrayList();
+        directors.add("Director 1");
+        directors.add("Director 2");
+
+
+        ArrayList<String> writers = new ArrayList();
+        writers.add("Writer 1");
+        writers.add("Writer 2");
+
+
+        ArrayList<String> cast1 = new ArrayList<>();
+        cast1.add("Actor A");
+        cast1.add("Actor B");
+
+        ArrayList<String> cast2 = new ArrayList<>();
+        cast2.add("Actor A");
+        cast2.add("Actor C");
+
+        Movie movie1 = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast1,
+                10);
+
+
+        Movie movie2 = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast2,
+                10);
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+
+        String mostPopularActor = HomeController.getMostPopularActor(movies);
+        assertEquals("Actor A", mostPopularActor);
     }
-*/
-        private HomeController homeController = new HomeController();
-
-        @Test
-        public void testFilteringByGenre() {
-            List<Movie> filteredMovies = homeController.getMoviesFiltered("", Genre.COMEDY);
-            assertEquals(5, filteredMovies.size()); // There are 5 comedy movies
-        }
-
-        @Test
-        public void testSortAscending() {
-            homeController.sortAscending();
-            List<Movie> sortedMovies = homeController.filteredMovies;
-            assertEquals("Annabelle", sortedMovies.get(0).getTitle()); // Annabelle is the first movie in asc sort
-        }
-
-        @Test
-        public void testSortDescending() {
-            homeController.sortDescending();
-            List<Movie> sortedMovies = homeController.filteredMovies;
-            assertEquals("Titanic", sortedMovies.get(0).getTitle());
-        }
 
 
+    @Test
+    void testGetLongestMovieTitle() {
+
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+        ArrayList<String> directors = new ArrayList();
+        directors.add("Director 1");
+        directors.add("Director 2");
+        ArrayList<String> writers = new ArrayList();
+        writers.add("Writer 1");
+        writers.add("Writer 2");
+
+
+        ArrayList<String> cast = new ArrayList<>();
+        cast.add("Actor A");
+        cast.add("Actor B");
+        cast.add("Actor C");
+
+        Movie movie = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast,
+                10);
+        Movie movie1 = new Movie("id1", "title111", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast,
+                10);
+        Movie movie2 = new Movie("id1", "title11111", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast,
+                10);
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie);
+        movies.add(movie1);
+        movies.add(movie2);
+
+
+        int longestTitleLength = HomeController.getLongestMovieTitle(movies);
+
+        assertEquals(10, longestTitleLength);
+    }
+
+    @Test
+    void testGetLongestMovieTitle_EmptyList() {
+        List<Movie> movies = new ArrayList<>();
+
+        int longestTitleLength = HomeController.getLongestMovieTitle(movies);
+
+        assertEquals(0, longestTitleLength);
+    }
+
+    @Test
+    public void testCountMoviesFromWithEmptyMoviesList() {
+        assertEquals(0, HomeController.countMoviesFrom(new ArrayList<>(), "Director"));
+    }
+
+    @Test
+    public void testCountMoviesFrom() {
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+        ArrayList<String> directors = new ArrayList();
+        directors.add("Director 1");
+        directors.add("Director 2");
+
+        ArrayList<String> writers = new ArrayList();
+        writers.add("Writer 1");
+        writers.add("Writer 2");
+
+
+        ArrayList<String> cast1 = new ArrayList<>();
+        cast1.add("Actor A");
+        cast1.add("Actor B");
+        cast1.add("Actor A");
+        Movie movie1 = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                writers,
+                10);
+        Movie movie2 = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast1,
+                10);
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+
+
+        assertEquals(2, HomeController.countMoviesFrom(movies, "Director 1"));
+    }
+
+    // Test case 1: Get movies between 1990 and 2000
+    @Test
+    public void testGetMoviesBetweenYears_Case1() {
+
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+        ArrayList<String> directors = new ArrayList();
+        directors.add("thomas");
+        directors.add("trim");
+
+        ArrayList<String> writers = new ArrayList();
+        writers.add("thomas");
+        writers.add("trim");
+
+
+        ArrayList<String> cast1 = new ArrayList<>();
+        cast1.add("Actor A");
+        cast1.add("Actor B");
+        cast1.add("Actor A");
+        Movie movie1 = new Movie("id1", "title1", genres,
+                1995, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                writers,
+                10);
+        Movie movie2 = new Movie("id1", "title1", genres,
+                2002, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast1,
+                10);
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+
+        List<Movie> result1 = HomeController.getMoviesBetweenYears(movies, 1990, 2000);
+        Assertions.assertEquals(1, result1.size());
+        Assertions.assertTrue(result1.contains(movie1));
+    }
+
+    // Test case 2: Get movies between 2010 and 2020
+    @Test
+    public void testGetMoviesBetweenYears_Case2() {
+
+        ArrayList<Genre> genres = new ArrayList();
+        genres.add(Genre.ROMANCE);
+        genres.add(Genre.ADVENTURE);
+        genres.add(Genre.CRIME);
+        ArrayList<String> directors = new ArrayList();
+        directors.add("Director 1");
+        directors.add("Director 2");
+
+        ArrayList<String> writers = new ArrayList();
+        writers.add("Writer 1");
+        writers.add("Writer 2");
+
+
+        ArrayList<String> cast1 = new ArrayList<>();
+        cast1.add("Actor A");
+        cast1.add("Actor B");
+        cast1.add("Actor A");
+        Movie movie1 = new Movie("id1", "title1", genres,
+                1992, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                writers,
+                10);
+        Movie movie2 = new Movie("id1", "title1", genres,
+                1997, "description",
+                "https://image.com/",
+                120,
+                directors,
+                writers,
+                cast1,
+                10);
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+        movies.add(movie2);
+
+        // Test case 1: Get movies between 1990 and 2000
+        List<Movie> result1 = HomeController.getMoviesBetweenYears(movies, 1990, 2000);
+        Assertions.assertEquals(2, result1.size());
+        Assertions.assertTrue(result1.contains(movie1));
+        Assertions.assertTrue(result1.contains(movie2));
+
+    }
 }
